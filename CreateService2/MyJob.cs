ec422b346d98432776c7e196ad2e8f3f488943ac
Jürgen Maczioch <juergen.maczioch@outlook.de>
2019-02-13 15:59:01 +0100

    Projektdateien hinzufügen.

---
+++ CreateService2/MyJob.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Quartz;
+
+namespace CreateService2
+{
+    public class MyJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            var pathTarget  = Settings.Default.PathTarget;
+            var pathSource  = Settings.Default.PathSource;
+            var filePattern = Settings.Default.FilePattern;
+            
+            Console.WriteLine($"[{DateTime.UtcNow}] Welcome jmac2050 filecopy ...");
+
+            var files = Directory.GetFiles(pathSource, filePattern);
+
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                var fullFileNameTarget = Path.Combine(pathTarget, fileName);
+
+                if (!File.Exists(fullFileNameTarget))
+                    File.Copy(file, fullFileNameTarget);
+            }
+        }
+    }
+}
---
+++ CreateService2/Program.cs
@@ -0,0 +1,51 @@
+﻿using Quartz;
+using Topshelf;
+using Topshelf.Quartz;
+
+namespace CreateService2
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<MyService>(s =>
+                {
+                    s.WhenStarted(service => service.OnStart());
+                    s.WhenStopped(service => service.OnStop());
+                    s.ConstructUsing(()   => new MyService());
+
+                    s.ScheduleQuartzJob(q =>
+                        q.WithJob(() =>
+                                JobBuilder.Create<MyJob>().Build())
+                            .AddTrigger(() => TriggerBuilder.Create()
+                                .WithSimpleSchedule(b => b
+                                    .WithIntervalInSeconds(Settings.Default.Intervall)
+                                    .RepeatForever())
+                                .Build()));
+                });
+
+                x.RunAsLocalSystem()
+                    .DependsOnEventLog()
+                    .StartAutomatically()
+                    .EnableServiceRecovery(rc => rc.RestartService(1));
+
+                x.SetServiceName("My Topshelf Service");
+                x.SetDisplayName("My Topshelf Service");
+                x.SetDescription("My Topshelf Service's description");
+            });
+        }
+    }
+
+    public class MyService
+    {
+        public void OnStart()
+        {
+        }
+
+        public void OnStop()
+        {
+        }
+    }
+}
